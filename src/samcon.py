7fe327f22a693bffd293416503fe9417c670dd41
liangpan-github <liangpan1005@163.com>
2021-08-20 19:23:30 +0800

    change Chinese code annotations to English

--- src/samcon.py
+++ src/samcon.py
@@ -14,7 +14,7 @@ SAMPLED_TARGET_STATE_INDEX = 3
 SIM_TARGET_STATE_INDEX = 4
 COST_INDEX = 5
 
-offset = 0.0 # 0.2 # 时间偏移
+offset = 0.0 # sample time offset
 
 class SamCon():
 
@@ -25,14 +25,12 @@ class SamCon():
         self._sampleTimeStep = sampleTimeStep
         self._savePath = savePath
 
-
-
     def learn(self, nIter, nSample, nSave, nSaveFinal, dataPath, displayFPS, useFPS):
         
-        print('SamCon 开始训练')
+        print('SamCon Start Learn')
 
         self._mocap_data = PybulletMocapData(dataPath, self._pb_client)
-        print('序列的持续时间=', self._mocap_data.getCycleTime())
+        print('Motion Sequence Cycle Time = ', self._mocap_data.getCycleTime())
 
         self._nIter = nIter
         self._nSample = nSample
@@ -43,39 +41,40 @@ class SamCon():
         assert self._nSaveFinal <= self._nSave
 
         startTime = time.clock()
-        """
-        
-        [initBulletState, endBulletState, targetState, sampledTargetState, simTargetState, cost]
 
+        """ 
+        Data form in SM:
+        [initBulletState, endBulletState, targetState, sampledTargetState, simTargetState, cost]
         """
         SM = []
 
         # initialize SM[0]
         SM.append([])
-        firstInitState = self._mocap_data.getSpecTimeState(t=0.0+offset)
+        firstInitState = self._mocap_data.getSpecTimeState(t = 0.0 + offset)
         for i in range(self._nSave):
             SM[0].append([0.0, firstInitState, 0.0, 0.0, 0.0, 0.0])
 
-        for t in range(1, self._nIter+1):
+        for t in range(1, self._nIter + 1):
             
-            nInitState = len(SM[t-1])
+            nInitState = len(SM[t - 1])
             nSampleEachInitState = int(self._nSample / nInitState)
 
-            target_state = self._mocap_data.getSpecTimeState(t*self._sampleTimeStep+offset)
+            target_state = self._mocap_data.getSpecTimeState(t * self._sampleTimeStep + offset)
             S = []
             cost_list = []
-            for state_set in SM[t-1]:
+            for state_set in SM[t - 1]:
                 init_bullet_state = state_set[END_BULLET_STATE_INDEX]
 
                 for i in range(nSampleEachInitState):
                     
-                    # 必须用这种方式加载pybullet world state
-                    if t != 1:
-                        self._pb_client.restoreState(init_bullet_state)
+                    # load last time simulation result
+                    # use pybullet API: restoreState to ensure the continuity between two successive simulations 
                     if t == 1:
                         self._humanoid.resetState(init_bullet_state, None)
+                    if t != 1:
+                        self._pb_client.restoreState(init_bullet_state)
 
-                    sampled_target_state = self.sample(target_state) # 只修改pose
+                    sampled_target_state = self.sample(target_state) 
                     self._humanoid.resetState(None, target_state)
                     sim_target_state, cost = self._humanoid.simulation(sampled_target_state, self._sampleTimeStep, displayFPS, useFPS)
                     end_bullet_state = self._pb_client.saveState()
@@ -83,26 +82,16 @@ class SamCon():
                     
                     cost_list.append(cost)
             
-            # # 从nSample个样本中挑nSave个最小的保存
-            # cost_list = np.array(cost_list)
-            # cost_order = cost_list.argsort() # 从小到大排序 返回索引
-            # cost_order = cost_order[0:self._nSave] # 取前nSave个
-
-            # SM.append([])
-            # for index in cost_order:
-            #     SM[t].append(S[index])
-            
-            ## 从nSample个样本中挑nSave个保存
-            ## cost distribution method
-            # 1. 丢弃最高的40%个样本
+            ## select nSave of the nSample samples to save
+            # 1. discard samples lying in the top 40% of the cost distribution
             cost_list = np.array(cost_list)
-            cost_order = cost_list.argsort() # 从小到达排序 返回索引
+            cost_order = cost_list.argsort() # sort from small to large
             numDiscard = int(self._nSample * 0.6)
-            cost_order = cost_order[0:numDiscard] # 保存前60%
-            # 2. 找到cost_min和cost_max
+            cost_order = cost_order[0 : numDiscard]
+            # 2. find new cost_min, cost_max
             cost_min = cost_list[cost_order[0]]
             cost_max = cost_list[cost_order[-1]]
-            # 3. 寻找nSave个样本保存
+            # 3. select nSave samples
             indices_list = []
             for x_i in range(self._nSave):
                 x = x_i / self._nSave
@@ -119,20 +108,22 @@ class SamCon():
             time_so_far = (time.clock() - startTime)
             print('iter: {:d}  time_so_far:  {:.2f}s'.format(t, time_so_far))
 
-        ## 从SM的尾部开始回溯, 找到nSave个path, 求他们的total_cost, 保存nSaveFinal个total_cost最小的结果
-        # 找path
+        ## select nSaveFinal of nSave paths to save
+        # 1. search nSave paths
+        # from the element of SM at the last time step of a motion, we backtrack nSave paths
         path_list = []
         for pathId in range(self._nSave):
             path = []
             path.append(pathId)
 
             currTime_initBulletState = SM[-1][pathId][INIT_BULLET_STATE_INDEX]
-            for i in range(self._nIter-1): # 只需要比较nIter-1次
+            for i in range(self._nIter - 1): # only need to compare nIter-1 times
                 lastTime = -2 - i
                 for j in range(self._nSave):
                     lastTime_endBulletState = SM[lastTime][j][END_BULLET_STATE_INDEX]
-                    # 当前时刻的初始状态等于上一时刻的仿真结果, 则说明两者属于同一条path
-                    # 使用唯一标记来标识每次的仿真
+
+                    # if the current time's init state equal to the last time's end state,
+                    # it proves that two samples belong to the same path.
                     if lastTime_endBulletState == currTime_initBulletState:
                         path.insert(0, j)
                         currTime_initBulletState = SM[lastTime][j][INIT_BULLET_STATE_INDEX]
@@ -140,38 +131,32 @@ class SamCon():
                     
             path_list.append(path)
 
-
-        # 求每条path的total_cost
+        # 2. calculate the total cost of each path
         total_cost_list = []
         for pathId in range(self._nSave):
             total_cost = 0
-            for t in range(1, self._nIter+1):
-                savedSample_index = path_list[pathId][t-1]
+            for t in range(1, self._nIter + 1):
+                savedSample_index = path_list[pathId][t - 1]
                 total_cost += SM[t][savedSample_index][COST_INDEX]
             total_cost_list.append(total_cost)
-        
         total_cost_list = np.array(total_cost_list)
-        order = total_cost_list.argsort() # 从小到大排序 返回索引
-        order = order[0:self._nSaveFinal] # 取前nSaveFinal个最小的
-        
 
+        # 3. save nSaveFinal paths with minimum total cost
+        order = total_cost_list.argsort() 
+        order = order[0 : self._nSaveFinal]
         self.save(SM, path_list, total_cost_list, order)
 
-
-       
+        print('SamCon End Learn')
         exit()
 
 
     def test(self, dataPath, displayFPS, useFPS):
 
-        print('SamCon 开始测试')
+        print('SamCon Start Test')
 
-        # 加载txt文件
+        # load txt file
         with open(dataPath, 'r') as f:
             data = json.load(f)
-
-        datasetPath = data['info']['dataset_path']
-        mocap_data = PybulletMocapData(datasetPath, self._pb_client)
 
         nIter = data['info']['nIter']
         nSaveFinal = data['info']['nSaveFinal']
@@ -190,20 +175,20 @@ class SamCon():
             assert len(sampled_states) == nIter
             assert len(simulated_states) == nIter
 
-            self._humanoid.resetState(start_state=self.getStateFromList(t0_state), end_state=None)
+            self._humanoid.resetState(start_state = self.getStateFromList(t0_state), end_state = None)
             
-            for t in range(1, nIter+1):
+            for t in range(1, nIter + 1):
                 print('iter:  ', t)
-                reference_state = reference_states[t-1]
+                reference_state = reference_states[t - 1]
                 reference_state = self.getStateFromList(reference_state)
 
-                sampled_state = sampled_states[t-1]
+                sampled_state = sampled_states[t - 1]
                 sampled_state = self.getStateFromList(sampled_state)
 
-                simulated_state = simulated_states[t-1]
+                simulated_state = simulated_states[t - 1]
                 simulated_state = self.getStateFromList(simulated_state)
 
-                self._humanoid.resetState(start_state=None, end_state=reference_state)
+                self._humanoid.resetState(start_state = None, end_state = reference_state)
                 state, _ = self._humanoid.simulation(sampled_state, sampleTimeStep, displayFPS, useFPS)
 
                 state_1 = self.getListFromNamedtuple(state)
@@ -212,20 +197,79 @@ class SamCon():
                 state_2 = np.array(state_2)
                 print('error: ', (state_1-state_2).sum())
                 # time.sleep(0.5)
+    
+    def save(self, SM, path_list, pathCost_list, savedIndices):
+        """ save as txt file """
+        assert savedIndices.shape[0] == self._nSaveFinal
+        assert len(path_list) == self._nSave
+        assert len(pathCost_list) == self._nSave
+
+        data = {
+            
+        }
+
+        info = {
+            'dataset_path': self._mocap_data.DataPath(),
+            'nIter': self._nIter,
+            'nSample': self._nSample,
+            'nSave': self._nSave,
+            'nSaveFinal': self._nSaveFinal,
+            'sampleTimeStep': self._sampleTimeStep,
+            'simulationTimeStep': self._simTimeStep
+        }
+        data['info'] = info
+
+        for i in range(self._nSaveFinal):
+            pathId = savedIndices[i]
+            
+            # save the init state at t=0
+            t0_state = SM[0][0][END_BULLET_STATE_INDEX]
+            t0_state = self.getListFromNamedtuple(t0_state)
+
+            # save the other states after t=0
+            reference_states = []
+            sampled_states = []
+            simulated_states = []
+            for t in range(1, self._nIter + 1):
+                savedSample_index = path_list[pathId][t - 1]
+                reference_state = SM[t][savedSample_index][TARGET_STATE_INDEX]
+                sampled_state = SM[t][savedSample_index][SAMPLED_TARGET_STATE_INDEX]
+                simulated_state = SM[t][savedSample_index][SIM_TARGET_STATE_INDEX]
+
+                reference_state = self.getListFromNamedtuple(reference_state)
+                sampled_state = self.getListFromNamedtuple(sampled_state)
+                simulated_state = self.getListFromNamedtuple(simulated_state)
+
+                reference_states.append(reference_state)
+                sampled_states.append(sampled_state)
+                simulated_states.append(simulated_state)
+
+
+            dataUnit = {
+                'cost': pathCost_list[pathId],
+                't0_state': t0_state,
+                'reference_states': reference_states,
+                'sampled_states': sampled_states,
+                'simulated_states': simulated_states,
+            }
+
+            data[f'path_{i}'] = dataUnit
+    
+        f = open(self._savePath, 'w')
+        json.dump(data, f)
+        f.close()
+        print(f'file saved! [{self._savePath}]')
                 
 
-        
-
     def genRandomFromUniform(self, samplingWindow):
-        """ 从均匀分布中生成随机数, 返回一个和samplingWindow大小一样的列表 """
+        """ Generate random number from uniform distribution. """
         diff = []
         for i in samplingWindow:
-            diff.append(np.random.uniform(low=-i, high=i, size=(1)))
+            diff.append(np.random.uniform(low = -i, high = i, size = (1)))
         return diff
     
     def genModifiedQuaternion(self, quaternion, samplingWindow):
         if len(quaternion) == 4:
-
             euler = self._pb_client.getEulerFromQuaternion(quaternion)
             diff = self.genRandomFromUniform(samplingWindow)
             eulerDiff = (
@@ -242,8 +286,12 @@ class SamCon():
         return quaternionDiff
 
     def sample(self, state):
+        """ Perform random sampling on rotation of all movable joints to produce a new pose. """
 
         modified_state = state._replace(
+            basePos = [0, 0, 0],
+            baseOrn = [0, 0, 0, 1],
+
             chestRot=self.genModifiedQuaternion(state.chestRot, c.samplingWindow[0]),
             neckRot=self.genModifiedQuaternion(state.neckRot, c.samplingWindow[1]),
             rightHipRot=self.genModifiedQuaternion(state.rightHipRot, c.samplingWindow[2]),
@@ -274,79 +322,16 @@ class SamCon():
         )
 
         return modified_state
-        
-    
-    def save(self, SM, path_list, pathCost_list, savedIndices):
-        """ 保存为.txt """
-        assert savedIndices.shape[0] == self._nSaveFinal
-        assert len(path_list) == self._nSave
-        assert len(pathCost_list) == self._nSave
-
-        data = {
-            
-        }
-
-        info = {
-            'dataset_path': self._mocap_data.DataPath(),
-            'nIter': self._nIter,
-            'nSample': self._nSample,
-            'nSave': self._nSave,
-            'nSaveFinal': self._nSaveFinal,
-            'sampleTimeStep': self._sampleTimeStep,
-            'simulationTimeStep': self._simTimeStep
-        }
-        data['info'] = info
-
-        for i in range(self._nSaveFinal):
-            pathId = savedIndices[i]
-            
-
-            # 保存t=0的初始状态
-            t0_state = SM[0][0][END_BULLET_STATE_INDEX]
-            t0_state = self.getListFromNamedtuple(t0_state)
-
-            # 保存t=0之后时刻的reference states, sampled_states, simulated states
-            reference_states = []
-            sampled_states = []
-            simulated_states = []
-            for t in range(1, self._nIter+1):
-                savedSample_index = path_list[pathId][t-1]
-                reference_state = SM[t][savedSample_index][TARGET_STATE_INDEX]
-                sampled_state = SM[t][savedSample_index][SAMPLED_TARGET_STATE_INDEX]
-                simulated_state = SM[t][savedSample_index][SIM_TARGET_STATE_INDEX]
-
-                reference_state = self.getListFromNamedtuple(reference_state)
-                sampled_state = self.getListFromNamedtuple(sampled_state)
-                simulated_state = self.getListFromNamedtuple(simulated_state)
-
-                reference_states.append(reference_state)
-                sampled_states.append(sampled_state)
-                simulated_states.append(simulated_state)
-
-
-            dataUnit = {
-                'cost': pathCost_list[pathId],
-                't0_state': t0_state,
-                'reference_states': reference_states,
-                'sampled_states': sampled_states,
-                'simulated_states': simulated_states,
-            }
-
-            data[f'path_{i}'] = dataUnit
-    
-        f = open(self._savePath, 'w')
-        json.dump(data, f)
-        f.close()
-        print(f'file saved! [{self._savePath}]')
 
     def getListFromNamedtuple(self, namedtuple, is_pose=True, is_velocity=True):
-        """ 从collections.namedtuple类型的state转换为列表 
+        """ Type transition: from collections.namedtuple to list.
         
         Return:
             pose len=43
             velocity len=34
             pose+velocity len=77
         """
+
         if is_pose:
             pose = list(namedtuple.basePos) + list(namedtuple.baseOrn) \
                  + list(namedtuple.chestRot) + list(namedtuple.neckRot) \
@@ -367,50 +352,44 @@ class SamCon():
         else:
             velocity = []
         
-        return pose+velocity
+        return pose + velocity
 
 
     def getStateFromList(self, data):
-        """ 从列表转换为collections.namedtuple类型的state
-        
-        Return:
-            pose len=43
-            velocity len=34
-            pose+velocity len=77
-        """
+        """ Type transition: from list to collections.namedtuple. """
 
         state = State(
         # Position
-        basePos = data[0:3],
-        baseOrn = data[3:7],
-        chestRot = data[7:11], 
-        neckRot = data[11:15], 
-        rightHipRot = data[15:19], 
-        rightKneeRot = data[19:20], 
-        rightAnkleRot = data[20:24], 
-        rightShoulderRot = data[24:28], 
-        rightElbowRot = data[28:29], 
-        leftHipRot = data[29:33], 
-        leftKneeRot = data[33:34], 
-        leftAnkleRot = data[34:38], 
-        leftShoulderRot = data[38:42], 
-        leftElbowRot = data[42:43],
+        basePos = data[0 : 3],
+        baseOrn = data[3 : 7],
+        chestRot = data[7 : 11], 
+        neckRot = data[11 : 15], 
+        rightHipRot = data[15 : 19], 
+        rightKneeRot = data[19 : 20], 
+        rightAnkleRot = data[20 : 24], 
+        rightShoulderRot = data[24 : 28], 
+        rightElbowRot = data[28 : 29], 
+        leftHipRot = data[29 : 33], 
+        leftKneeRot = data[33 : 34], 
+        leftAnkleRot = data[34 : 38], 
+        leftShoulderRot = data[38 : 42], 
+        leftElbowRot = data[42 : 43],
 
         # Velocity
-        baseLinVel = data[43:46],
-        baseAngVel = data[46:49],
-        chestVel = data[49:52], 
-        neckVel = data[52:55], 
-        rightHipVel = data[55:58], 
-        rightKneeVel = data[58:59], 
-        rightAnkleVel = data[59:62], 
-        rightShoulderVel = data[62:65], 
-        rightElbowVel = data[65:66], 
-        leftHipVel = data[66:69], 
-        leftKneeVel = data[69:70], 
-        leftAnkleVel = data[70:73], 
-        leftShoulderVel = data[73:76], 
-        leftElbowVel = data[76:77],
+        baseLinVel = data[43 : 46],
+        baseAngVel = data[46 : 49],
+        chestVel = data[49 : 52], 
+        neckVel = data[52 : 55], 
+        rightHipVel = data[55 : 58], 
+        rightKneeVel = data[58 : 59], 
+        rightAnkleVel = data[59 : 62], 
+        rightShoulderVel = data[62 : 65], 
+        rightElbowVel = data[65 : 66], 
+        leftHipVel = data[66 : 69], 
+        leftKneeVel = data[69 : 70], 
+        leftAnkleVel = data[70 : 73], 
+        leftShoulderVel = data[73 : 76], 
+        leftElbowVel = data[76 : 77],
         )
         
         return state
